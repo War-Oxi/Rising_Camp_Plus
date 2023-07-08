b92e5f4f9154a2fc589b69ba492142edd084f589
Oxi <xowl5460@naver.com>
2023-07-09 07:12:39 +0900

    BOJ_10172

--- BackJoon/BOJ_10171/src/Main.java
+++ BackJoon/BOJ_10171/src/Main.java
@@ -1,13 +1,10 @@
 import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-		System.out.println(
-			"|\\_/|\n" +
-			"|q p|   /}\n" +
-			"( 0 )\"\"\"\\\n" +
-			"|\"^\"`    |\n" +
-			"||_/=\\\\__|"
-		);
+		System.out.println("\\    /\\\n" +
+			" )  ( ')\n" +
+			"(  /  )\n" +
+			" \\(__)|");
 	}
 }
 
