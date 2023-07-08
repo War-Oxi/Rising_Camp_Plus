0c813e1b9e55f295c580d711be0aed397d5e8a47
Oxi <xowl5460@naver.com>
2023-07-09 07:47:49 +0900

    BOJ_14681

---
+++ BackJoon/BOJ_14681/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int xPos = sc.nextInt();
+		int yPos = sc.nextInt();
+
+		if(xPos > 0){
+			if(yPos > 0){
+				System.out.println(1);
+			} else {
+				System.out.println(4);
+			}
+		} else {
+			if(yPos > 0){
+				System.out.println(2);
+			} else {
+				System.out.println(3);
+			}
+		}
+	}
+}
