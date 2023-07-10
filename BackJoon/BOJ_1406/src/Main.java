282454d0a4cae8bc33c93437f10403bd73cd08c0
War-Oxi <xowl5460@naver.com>
2023-07-10 18:17:53 +0900

    BOJ_1406

---
+++ BackJoon/BOJ_1406/src/Main.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+//        LinkedList<Character> Editor = new LinkedList<>();
+        Stack<Character> Editor = new Stack<>();
+        Stack<Character> tmpStack = new Stack<>();
+
+        String inputText = br.readLine();
+        for(int i=0; i<inputText.length(); i++){
+            Editor.add(inputText.charAt(i));
+        }
+
+        int numOfCommand = Integer.parseInt(br.readLine());
+        int idx = Editor.size();
+
+        for(int i=0; i<numOfCommand; i++){
+            String Command = br.readLine();;
+            if(Command.charAt(0) == 'L'){
+                if(idx == 0) {
+                    continue;
+                }
+                tmpStack.add(Editor.pop());
+                idx--;
+            } else if (Command.charAt(0) == 'D') {
+                if(idx == Editor.size() + tmpStack.size()) {
+                    continue;
+                }
+                Editor.add(tmpStack.pop());
+                idx++;
+            } else if (Command.charAt(0) == 'B') {
+                if(idx == 0) {
+                    continue;
+                }
+                Editor.pop();
+                idx--;
+            } else if (Command.charAt(0) == 'P') {
+                Editor.add(idx, Command.charAt(2));
+                idx++;
+            } else {
+                bw.write("잘못된 명력어 입력. 프로그램 종료");
+                return;
+            }
+        }
+        while(!tmpStack.empty()){
+            Editor.push(tmpStack.pop());
+        }
+        for (Character ele : Editor) {
+            bw.write(ele);
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
